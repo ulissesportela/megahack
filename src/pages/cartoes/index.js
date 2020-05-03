bc4c74739be7f526649f38b14fb329c7c1e1bd8c
Ulisses Douglas <ulisses.portela@gmail.com>
2020-05-03 13:56:48 -0300

    Atualizando icones e incluindo FlatList na lista

--- src/pages/cartoes/index.js
+++ src/pages/cartoes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, FlatList, Text, Image, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import logoImg from '../assets/logo.png'
 import styles from './styles'
@@ -16,7 +16,12 @@ export default function Cartoes() {
             <Text style={styles.title}>Bem-vindo!</Text>
             <Text style={styles.description}>Escolha um dos cartões abaixo e inicie um network.</Text>
 
-            <View style={styles.cartaoList} >
+            <FlatList
+                style={styles.cartaoList} 
+                data={[1,2,3]}
+                keyExtractor={cartao => String(cartao)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
                     <View style={styles.cartao} >
                         <Text style={styles.cartaoProperty}>Nome:</Text> 
                         <Text style={styles.cartaoValue}>Lorie Smith</Text> 
@@ -32,39 +37,9 @@ export default function Cartoes() {
                             <Icon name='info-circle' size={16} color="#83DEA3" />
                         </TouchableOpacity>
                     </View>
+                )}
+            />
 
-                    <View style={styles.cartao} >
-                        <Text style={styles.cartaoProperty}>Nome:</Text> 
-                        <Text style={styles.cartaoValue}>Lorie Smith</Text> 
-
-                        <Text style={styles.cartaoProperty}>Bio:</Text> 
-                        <Text style={styles.cartaoValue}>Gestora de créditos da XYZ Bank, onde a Lorie processa os pedidos de empréstimo do início ao fim, incluindo o refinanciamento de hipotecas e educando os clientes sobre suas diferentes opções de financiamento.</Text> 
-
-                        <Text style={styles.cartaoProperty}>Tipo de network:</Text> 
-                        <Text style={styles.cartaoValue}>Profissional</Text> 
-
-                        <TouchableOpacity style={styles.detailsButton}>
-                            <Text style={styles.detailsButtonText}>Ver mais datalhes</Text>
-                            <Icon name='arrow-right' size={16} color="#83DEA3" />
-                        </TouchableOpacity>
-                    </View>
-
-                    <View style={styles.cartao} >
-                        <Text style={styles.cartaoProperty}>Nome:</Text> 
-                        <Text style={styles.cartaoValue}>Lorie Smith</Text> 
-
-                        <Text style={styles.cartaoProperty}>Bio:</Text> 
-                        <Text style={styles.cartaoValue}>Gestora de créditos da XYZ Bank, onde a Lorie processa os pedidos de empréstimo do início ao fim, incluindo o refinanciamento de hipotecas e educando os clientes sobre suas diferentes opções de financiamento.</Text> 
-
-                        <Text style={styles.cartaoProperty}>Tipo de network:</Text> 
-                        <Text style={styles.cartaoValue}>Profissional</Text> 
-
-                        <TouchableOpacity style={styles.detailsButton}>
-                            <Text style={styles.detailsButtonText}>Ver mais datalhes</Text>
-                            <Icon name='arrow-right' size={16} color="#83DEA3" />
-                        </TouchableOpacity>
-                    </View>
-            </View>
         </View>
     )
 }
