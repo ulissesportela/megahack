aabca08ee428af7d97d5b636b43d77d90b26f133
Ulisses Douglas <ulisses.portela@gmail.com>
2020-05-03 02:06:58 -0300

    Instalando react-navigation, e criando as rotas

--- App.js
+++ App.js
@@ -1,24 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
 import React from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import Routes from './src/routes'
 
 const App = () => {
   return (
-    <>
-      <View>
-        <Text>Hello Tarrafa</Text>
-      </View>
-    </>
+    <Routes/>
   );
 };
 
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Cartoes from './pages/cartoes'
+import Cartao from './pages/cartao'
+
+const AppStack = createStackNavigator()
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator>
+                <AppStack.Screen name="Cartoes" component={Cartoes} />
+                <AppStack.Screen name="Cartao" component={Cartao} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
+}
